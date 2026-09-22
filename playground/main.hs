301a051f5a457df0f1467c866b675aa1afa42cec
DizzyLaptop <mkulvanovskis@gmail.com>
2026-09-22 17:41:27 +0300

    get more familiar with one liner code

--- playground/main.hs
+++ playground/main.hs
@@ -1,4 +1,13 @@
-main = interact $ myFunc
+main :: IO ()
+main = interact myFunc 
 
-myFunc :: Int -> Int
-myFunc x = x * x
+-- sum of two numbers
+myFunc :: String -> String
+myFunc input = show(mySum $ numbers(input))
+
+numbers :: String -> [Int]
+numbers input = map read (words input) 
+
+mySum :: [Int] -> Int
+mySum[] = 0
+mySum(x:xs) = x + mySum xs 
